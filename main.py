22ed0a4de38def0cc208c5efd77a080821da6401
nickplaysjazz <saunders.nicholas97@gmail.com>
2026-03-27 11:08:54 -0500

    feat: added settings.ini, basic error statements

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from calc-onsager-from-akmc!")
+from pathlib import Path
 
+from constants import expected_options, SinkGeometry
+from error_handler import raise_error
+from io_handler import read_settings
+
+def main():
+    # Read in values from settings.ini
+    settings = read_settings("settings.ini", expected_options)
+
+    if settings["sink_type"] == SinkGeometry.BULK or settings["sink_type"] == SinkGeometry.GRAIN_BOUNDARY:
+        raise_error(Path(__file__).name, f"Sink geometry {settings["sink_type"]} not implemented!")
+
+
+    print(settings)
 
 if __name__ == "__main__":
     main()
